4a9c79483f363b0b94c9b35b1380f2c4a8898e38
hjl <1617451312@qq.oom>
2016-07-14 00:20:22 +0800

    714

--- auth/auth.service.js
+++ auth/auth.service.js
@@ -2,6 +2,15 @@
  * Created by jialao on 2016/7/12.
  */
 
+var mongoose = require('mongoose');
+var passport = require('passport');
+var config = require('../config/env');
+var jwt = require('jsonwebtoken');
+var expressJwt = require('express-jwt');
+var compose = require('composable-middleware');
+var User = mongoose.model('User');
+
+
 
 exports.isAuthenticated = function(){
     
--- auth/index.js
+++ auth/index.js
@@ -6,12 +6,17 @@ var express = require('express');
 var passport = require('passport');
 var config = require('../config/env');
 var mongoose = require('mongoose');
-
+var User = mongoose.model('User');
+var auth = require('./auth.service');
 
 
 var router = express.Router();
+require('./local/passport').setup(User,config);
+require('./github/passport').setup(User,config);
+require('./qq/passport').setup(User,config);
 
-
-
+router.use('/local',require('./local'));
+router.use('/github',require('./github'));
+router.use('/qq',require('qq'));
 
 module.exports = router;
