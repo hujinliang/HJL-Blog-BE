21a37889fae54e63b217bae000026842ec5e85fe
hjl <1617451312@qq.oom>
2016-07-16 14:18:58 +0800

    ''

--- routes.js
+++ routes.js
@@ -4,17 +4,17 @@
 var path = require('path');
 
 module.exports = function(app){
-    app.use('./users',require('./api/user'));
+    app.use('/users',require('./api/user'));
     
-    app.use('./auth',require('./auth'));
+    app.use('/auth',require('./auth'));
     
-    app.use('./tags',require('./api/tags'));
+    app.use('/tags',require('./api/tags'));
     
-    app.use('./article',require('./api/article'));
+    app.use('/article',require('./api/article'));
     
-    app.use('./comment',require('./api/comment'));
+    app.use('/comment',require('./api/comment'));
     
-    app.use('./logs',require('./api/logs'));
+    app.use('/logs',require('./api/logs'));
     
     // app.use('/*',function(req,res,next){
     //     return res.render('index')
