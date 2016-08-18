eaa25ae4ee7f0ee16ec071d97f99256d4765c966
hujingliang <1617451312@qq.com>
2016-08-18 11:51:36 +0800

    'finish'

--- config/env/production.js
+++ config/env/production.js
@@ -4,9 +4,11 @@
 module.exports = {
     port:process.env.PORT || 8800,
     mongo:{
-        uri:'mongodb://hjml69351:hjml69293@ds041394.mlab.com:41394/user'
+        // uri:'mongodb://hjml69351:hjml69293@ds041394.mlab.com:41394/user'
+        uri:'mongodb://localhost/hjl-blog'
     },
     session:{
         cookie:  {maxAge: 60000*5}
     }
+    
 }
